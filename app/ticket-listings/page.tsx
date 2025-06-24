ac23dd34afba295d0b9e62f2255c239eb710b784
Frances Pasulohan <f.pasulohan@lmf.ventures>
2025-06-24 15:47:20 +0800

    create ticket listing layout and page

---
+++ app/ticket-listings/page.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { title } from "@/components/primitives";
+import Tickets from "@/components/Tickets";
+
+export default function TicketListingPage() {
+
+  return (
+    <div>
+      <h1 className={title()}>Ticket Listings</h1>
+
+      <div className="mt-8">
+        <Tickets />
+      </div>
+
+    </div>
+  );
+}
