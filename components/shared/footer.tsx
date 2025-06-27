937bda275ef0db0500ff7fd6194b4e182a0b7f27
Frances Pasulohan <f.pasulohan@lmf.ventures>
2025-06-27 12:18:12 +0800

    adjust footer stying

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -27,13 +27,14 @@ const LinksWrapper = () => {
   const visibleFooterItems = footerItems.filter((item) => !item.isHidden);
 
   return (
-    <div className="grid grid-cols-3 gap-4 md:gap-x-12 lg:gap-x-32 xl:gap-x-32 ">
+    <div className="grid grid-cols-3 gap-4 md:gap-x-12 lg:gap-x-32 xl:gap-x-32">
       {visibleFooterItems.map((item) => (
         <Link
           key={item.href}
-          className="text-default-600 hover:text-default-800"
+          className="text-default-600 hover:text-default-800 uppercase"
           href={item.href}
           underline="always"
+          size="sm"
         >
           {item.label}
         </Link>
@@ -49,13 +50,13 @@ const AppFooter = () => {
     <footer className="w-full flex-col items-center justify-center pt-3">
       <div className="container mx-auto max-w-7xl px-6 py-2">
         <div className="flex-row justify-between items-center sm:flex">
-          <div className="flex-col text-sm text-default-500 w-full sm:w-1/2 justify-items-center sm:justify-items-start mb-3 sm:mb-0">
+          <div className="flex-col text-sm text-default-500 w-full sm:w-1/3 justify-items-center sm:justify-items-start mb-3 sm:mb-0">
             <span>Powered by </span>
             <Link className="text-default-600 hover:text-default-800" href="/">
               Ticket.net
             </Link>
 
-            <div className="flex items-center gap-3 mt-2 ">
+            <div className="flex items-center gap-3 mt-2">
               <Link isExternal aria-label="Twitter" href={twitter}>
                 <TwitterIcon className="text-default-500" />
               </Link>
