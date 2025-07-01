9a28990f4b24ca95005482d76986e1026e1c2bfb
Frances Pasulohan <f.pasulohan@lmf.ventures>
2025-07-01 17:21:44 +0800

    change function name and adjust styles

--- app/ticket-listings/layout.tsx
+++ app/ticket-listings/layout.tsx
@@ -1,11 +1,11 @@
-export default function PricingLayout({
+export default function TicketListingLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-3xl text-center justify-center h-screen">
+    <section className="flex flex-col items-center justify-center gap-6 pb-10">
+      <div className="inline-block max-w-6xl text-center justify-center min-h-screen">
         {children}
       </div>
     </section>
