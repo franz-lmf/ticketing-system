5e749b1bdee624e0c571ca7970d559f0062091d0
Frances Pasulohan <f.pasulohan@lmf.ventures>
2025-07-02 15:54:45 +0800

    increase width of event-detail div

--- app/event-details/[slug]/layout.tsx
+++ app/event-details/[slug]/layout.tsx
@@ -5,7 +5,7 @@ export default function EventDetailsLayout({
 }) {
   return (
     <section className="flex flex-col items-center justify-center gap-4">
-      <div className="inline-block max-w-6xl text-center justify-center min-h-screen">
+      <div className="inline-block text-center justify-center min-h-screen w-full">
         {children}
       </div>
     </section>
