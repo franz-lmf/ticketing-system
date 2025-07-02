d1608fcda1a89323b79ee2d00e1580acbd0efae9
Frances Pasulohan <f.pasulohan@lmf.ventures>
2025-07-02 19:19:01 +0800

    create generate random id helper function

---
+++ helpers/generators.helpers.tsx
@@ -0,0 +1,7 @@
+import { nanoid } from "nanoid";
+
+export const generateRandomId = () =>
+  typeof globalThis.crypto !== "undefined" &&
+    typeof globalThis.crypto.randomUUID === "function"
+    ? globalThis.crypto.randomUUID()
+    : nanoid();
