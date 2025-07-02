8190d042cf5f23fdf53f4c45268aa113567731e1
Frances Pasulohan <f.pasulohan@lmf.ventures>
2025-07-02 19:20:40 +0800

    adjust styles

--- components/TicketInfo.tsx
+++ components/TicketInfo.tsx
@@ -5,36 +5,46 @@ import { TicketDetails } from "@/interfaces/ticket.interface";
 import styles from "@/styles/ticket-info.module.scss";
 
 const TicketInfo = (ticketDetail: TicketDetails) => {
-  const { imageUrl, description, title, location, date, time } = ticketDetail;
-  const {
-    ticketDetailsContainer,
-    body,
-    eyebrowHeadline,
-    headline,
-    description: descriptionClass,
-  } = styles;
+  const { imageUrl, description, title, location, date, time, price } =
+    ticketDetail;
+  const { body, headline, description: descriptionClass } = styles;
+  const ticketDetailsContainer = styles["ticket-details-container"];
+  const eyebrowHeadline = styles["eyebrow-headline"];
+  const ticketInfoImage = styles["ticket-info-image"];
 
   return (
     <div
-      className={`flex flex-col justify-between sm:flex-row ${ticketDetailsContainer}`}
+      className={`flex flex-col justify-between sm:flex-row max-w-72 md:max-w-full ${ticketDetailsContainer}`}
     >
       <div className="flex w-full md:w-auto justify-center">
         <Image
           alt="Card background"
-          className="z-0 w-64 min-w-72 h-full object-cover transition-transform duration-300 group-hover:scale-105 group-hover:brightness-75 group-hover:blur-sm rounded-t-xl rounded-b-none  mx-auto "
+          className={ticketInfoImage}
           src={imageUrl}
         />
       </div>
+      {/* md:w-[40%] lg:w-[45%] xl:w-[50%]  */}
       <div
-        className={`flex-col container text-right w-full md:w-[40%] xl:w-[50%] mt-10 md:mt-auto ${body}`}
+        className={`flex flex-col container text-left w-full max-w-72 mt-10 md:mt-auto mx-auto lg:mr-auto justify-between md:ml-8 ${body}`}
       >
-        <p className={eyebrowHeadline}>{description}</p>
-        <h1 className={headline}>{title}</h1>
-        <p className={`ml-2 ${descriptionClass}`}>{date}</p>
-        <p className={`ml-2 ${descriptionClass}`}>{time}</p>
-        <p className={`ml-2 ${descriptionClass}`}>{location}</p>
+        <div>
+          <p className={eyebrowHeadline}>{description}</p>
+          {/* <h1 className={headline}>{title}</h1> */}
+          <h1 className="text-6xl lg:text-6xl font-bold capitalize my-3">{title}</h1>
+          <div className="">
+            <p className={descriptionClass}>{date}</p>
+            <p className={descriptionClass}>{time}</p>
+            <p className={descriptionClass}>{location}</p>
+          </div>
+        </div>
+        <h1 className="my-5 md:mt-auto">
+          <span className="font-light text-4xl md:text-5xl mr-1 mt-5">₱</span>
+          <span className="font-normal text-custom-accent text-6xl md:text-7xl">
+            {price.toFixed(2)}
+          </span>
+        </h1>
       </div>
-      <div>
+      <div className="bg-custom-accent min-w-14 w-48">
         <p>Buy Tickets</p>
       </div>
     </div>
