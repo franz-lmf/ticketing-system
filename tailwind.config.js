ce321437ba192e12a8e570d4e688cc7ad031c923
Frances Pasulohan <f.pasulohan@lmf.ventures>
2025-07-04 16:19:46 +0800

    finalize color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,12 +14,18 @@ const config = {
         mono: ["var(--font-mono)"],
       },
       colors: {
+        // overrite default colors
+        'primary': '#A60321',
+        'secondary': '#026414',
+        'success': '#026414',
+        'danger': '#dc3545',
+        'info': '#273890',
+        'warning': '#A63B05',
+
+        'text-background': '#161719',
+
+        // custom colors
         'custom-background': '#0E102D',
-        // 'custom-background-secondary': '#251728',
-        'custom-primary': '#0e102d',
-        'custom-secondary': '#1a1c3d',
-        'custom-accent': '#D1601E',
-        'custom-accent-secondary': '#273890',
         'custom-background-secondary': '#2B0F0E',
       }
     },
